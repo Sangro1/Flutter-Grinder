c51ca08c9b556c0c7885abb00ef149800d86b7a4
MY PC <mypc@192.168.1.126>
2022-07-13 10:52:42 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
+      home:RegisterPage(),
       theme: ThemeData(primarySwatch:Colors.grey),
     );
   }
